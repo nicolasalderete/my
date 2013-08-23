02e46b839e4c3aea03df827a055dbe0aea4f7665
nicolasalderete <nicolasalderete@gmail.com>
2013-08-23 00:32:35 -0300

    lala

--- .openshift/cron/daily/backup_db.sh
+++ .openshift/cron/daily/backup_db.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 NOW=$(date +"%m-%d-%Y")
 echo Comenzando con el backup de la base de datos
-mysqldump -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME --password=$OPENSHIFT_MYSQL_DB_PASSWORD my > $OPENSHIFT_REPO_DIR/../data/tmp/shcema.$NOW.log
+mysqldump -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME --password=$OPENSHIFT_MYSQL_DB_PASSWORD my > $OPENSHIFT_REPO_DIR/../data/tmp/shcema.$NOW.sql
 echo Enviando el mail a nicolasalderete@gmail.com
 cat $OPENSHIFT_REPO_DIR/../data/tmp/shcema.$NOW.sql | mail -s "BackUp" nicolasalderete@gmail.com
